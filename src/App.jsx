66bedf798c8ac8c2016eefaf7dfcff224a967cf4
Eduardo Vargas <vargased94@gmail.com>
2025-03-12 20:13:52 -0600

    add search functionality to movie app with input handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import { Movies } from "./Components/Movies";
 import { useMovies } from "./hooks/useMovies";
 
 function App() {
   const { movies } = useMovies();
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ query });
+  };
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  useEffect(() => {
+    if (query !== '') {}
+  }, [query]);
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de peliculas</h1>
-        <form className="form">
-          <input placeholder="Avengers, Star Wars, The Matrix ..." />
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            name="query"
+            ref={inputRef}
+            onChange={handleChange}
+            value={query}
+            placeholder="Avengers, Star Wars, The Matrix ..."
+          />
           <button type="submit">Buscar</button>
         </form>
       </header>
