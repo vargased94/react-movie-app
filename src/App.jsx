68b72c2b8c3b60f396c08e298f42a4d69a47cf77
Eduardo Vargas <vargased94@gmail.com>
2025-03-12 19:56:15 -0600

    initialize React movie search app with Vite setup and basic components

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { Movies } from "./Components/Movies";
+import { useMovies } from "./hooks/useMovies";
+
+function App() {
+  const { movies } = useMovies();
+
+  return (
+    <div className="page">
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form className="form">
+          <input placeholder="Avengers, Star Wars, The Matrix ..." />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+      <main>
+        <Movies movies={movies} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useMovies.js
@@ -0,0 +1,19 @@
+import responseMovies from "../mocks/movies.json";
+
+export function useMovies() {
+  const movies = responseMovies.Search;
+
+  /*
+    Esto se hace para no cambiar la estructura
+    de los datos en todos los componentes
+    y solo hacerlo en un solo lugar
+  **/
+  const mappedMovies = movies.map((movie) => ({
+    id: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    poster: movie.Poster,
+  }));
+
+  return { movies: mappedMovies };
+}
